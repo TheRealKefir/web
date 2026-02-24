549c01bc73ae7520f0a9b4bfa45a84115ec93e8a
therealkefir <therealkefir@mail.ru>
2026-02-24 18:27:43 +0500

    ready to mission

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+
+title = input()
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("base.html", title=title)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
